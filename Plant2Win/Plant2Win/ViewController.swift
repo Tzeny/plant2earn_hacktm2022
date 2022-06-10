1a4875031891c92b7bf3a219757e7adbdbcb0aaf
Stefan Iarca <stefan@Stefans-MacBook-Pro-5.local>
2022-06-10 21:37:55 +0300

    ios Commit

---
+++ Plant2Win/Plant2Win/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Plant2Win
+//
+//  Created by Stefan Iarca on 10.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
